d11f6bd57283f393a9965b18db10d652399fe3c0
LOEB-Dorian <131378149+LOEB-Dorian-2326069aa@users.noreply.github.com>
2025-11-04 08:34:50 +0100

    ajustement facturation

--- client/src/pages/invoice/invoiceCreation/invoiceSupportChoice/InvoiceSupportChoice.jsx
+++ client/src/pages/invoice/invoiceCreation/invoiceSupportChoice/InvoiceSupportChoice.jsx
@@ -144,7 +144,7 @@ const InvoiceSupportChoice = ({
             <svg className="fill-white size-[16px] " viewBox="0 -960 960 960">
               <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
             </svg>
-            Ajouter
+            Confirmer
           </button>
         </div>
       )}
