0518b8118b1762ada4d449fe3375f13610676c6d
MAALAL-Ines-2326071aa <ines.maalal@etu.univ-amu.fr>
2025-11-06 08:47:49 +0100

    fix problem mail

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -5,24 +5,25 @@ const mailerSend = new MailerSend({
   apiKey: process.env.EMAIL_API_KEY,
 });
 
-const sentFrom = new Sender(process.env.EMAIL_HOST, "V.A. Productions");
-
 export const sendMail = async (recipientEmail, subject, htmlContent, textContent) => {
-  const recipients = [new Recipient(recipientEmail, "Nouveau Membre")];
-
-  const emailParams = new EmailParams()
-    .setFrom(sentFrom)
-    .setTo(recipients)
-    .setReplyTo(sentFrom)
-    .setSubject(subject)
-    .setHtml(htmlContent) 
-    .setText(textContent);
 
   try {
-    console.log(`-> Tentative d'envoi d'email à ${recipientEmail}...`);
+    const sentFrom = new Sender(process.env.EMAIL_HOST, "V.A. Productions");
+    
+    const recipients = [new Recipient(recipientEmail, "Nouveau Membre")];
+
+    const emailParams = new EmailParams()
+      .setFrom(sentFrom)
+      .setTo(recipients)
+      .setReplyTo(sentFrom)
+      .setSubject(subject)
+      .setHtml(htmlContent)
+      .setText(textContent);
+
+    console.log(`Tentative d'envoi d'email à ${recipientEmail}...`);
     const response = await mailerSend.email.send(emailParams);
     console.log("Email envoyé avec succès ! Réponse de l'API :", response);
   } catch (error) {
-    console.error("Erreur lors de l'envoi de l'email:", error.body);
+    console.error(" Erreur lors de l'envoi de l'email :", error.body);
   }
 };
