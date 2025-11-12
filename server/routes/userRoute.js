831ad95b9d71c23d615f1f673d1eaf6a8e1fc9c8
LOEB-Dorian <dorian.loeb@etu.univ-amu.fr>
2025-11-12 11:26:46 +0100

    Ajout d'un bouton suppresion de compte

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,6 +5,7 @@ import {
   loginUser,
   verifyUser,
   updateUserRole,
+  deleteUser,
 } from "../controller/userController.js";
 import { authorize } from "../middleware/auth.js";
 import { Roles } from "../utils/Roles.js";
@@ -16,6 +17,8 @@ router.get("/", authorize(Roles.Admin), getAllUser);
 router.post("/create", authorize(Roles.Admin), createUser);
 
 router.put("/:userId/role", authorize(Roles.Admin), updateUserRole);
+
+router.delete("/:userId", authorize(Roles.Admin), deleteUser);
 
 router.post("/login", loginUser);
 
