59f0696f8e43d56b75bced3c2c85c579d9bd16d0
MAALAL-Ines-2326071aa <ines.maalal@etu.univ-amu.fr>
2025-11-17 12:39:30 +0100

    refactor filtre facture

--- client/src/pages/invoice/invoiceDisplay/InvoiceList.jsx
+++ client/src/pages/invoice/invoiceDisplay/InvoiceList.jsx
@@ -105,9 +105,15 @@ const InvoiceList = ({ invoices, setInvoices }) => {
                 <td className="table-cell-padding">
                   <div className="flex items-center justify-center space-x-2">
                     <span className={`h-2 w-2 rounded-full ${
-                      invoice.status === 'paid' ? 'bg-green-500' : isOverdue ? 'bg-red-500' : 'bg-yellow-500' // Pastille rouge plus foncée si en retard
+                      invoice.status === 'paid' ? 'bg-green-500' : isOverdue ? 'bg-red-500' : 'bg-yellow-500' 
                     }`}></span>
-                    <span>{invoice.status === "paid" ? "Payé" : "Non Payé"}</span>
+                    <span>
+                      {invoice.status === "paid" 
+                        ? "Payé" 
+                        : isOverdue 
+                          ? "Impayé" 
+                          : "Non Payé"}
+                    </span>
                   </div>
                 </td>
                 <td className="table-cell-padding">
