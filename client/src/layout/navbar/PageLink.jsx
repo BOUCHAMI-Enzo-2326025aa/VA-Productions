ddd15387ed3fb81d214673e0d4f570a6a055c079
tomvrdl <tverdilhan@icloud.com>
2025-11-18 19:06:36 +0100

    Fix de l'affichage 2

--- client/src/layout/navbar/PageLink.jsx
+++ client/src/layout/navbar/PageLink.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const PageLink = ({ link, icon, text, className }) => {
+const PageLink = ({ link, icon, text, className, closeNavbar }) => { // <--- PROPRIÉTÉ AJOUTÉE
   return (
     <Link
       className={
@@ -8,6 +8,7 @@ const PageLink = ({ link, icon, text, className }) => {
         " flex hover:bg-black hover:bg-opacity-5 opacity-70 hover:opacity-100 hover:scale-105 transition-all items-center rounded-md px-2 py-2 gap-4 text-white"
       }
       to={link}
+      onClick={closeNavbar} // <--- GESTIONNAIRE D'ÉVÉNEMENT AJOUTÉ
     >
       <img className="size-7 opacity-80" src={icon} />
       <p className="text-lg font-normal opacity-80">{text}</p>
